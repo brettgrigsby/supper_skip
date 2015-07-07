9b6d9078b2cb285e4df812c60ef091f25f4241a7
brettgrigsby <gbrett@gmail.com>
2015-07-06 19:46:15 -0600

    removed question mark from non-boolean method in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  before_action :authorize?
+  before_action :authorize
 
   def index
   end
@@ -8,7 +8,8 @@ class AdminController < ApplicationController
   end
 
   private
-  def authorize?
+
+  def authorize
     redirect_to "https://www.youtube.com/watch?v=4dGOfFbzvq4&t=0m45s" unless current_user.role == "admin"
   end
 end
