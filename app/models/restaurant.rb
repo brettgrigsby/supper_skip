4debc1bb0453e75f7905d6468eb68cd435b4f489
David Maurer <davemaurermail@gmail.com>
2015-07-07 08:20:48 -0600

    added validation tests for restaurant and factory file. all green.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+
   belongs_to :user
   has_many :items
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:restaurant) { create :restaurant }
+
+  describe "with valid params" do
+    it 'is valid' do
+      expect(restaurant).to be_valid
+    end
+
+    it 'has a unique name' do
+      create :restaurant
+      expect { create :restaurant }.to raise_exception ActiveRecord::RecordInvalid
+    end
+  end
 end
