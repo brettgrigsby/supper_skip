e5e6571f20ed8e363f883696531048bbc42453c5
David Maurer <davemaurermail@gmail.com>
2015-07-09 14:15:34 -0600

    finished validation tests for restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,19 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, :type => :model do
   let(:restaurant) { create :restaurant }
+  let(:item) { create :item }
+  let(:order) { create :order }
 
-  describe "with valid params" do
-    it 'is valid' do
-      expect(restaurant).to be_valid
-    end
+  it 'is valid' do
+    expect(restaurant).to be_valid
+  end
 
-    it 'has a unique name' do
-      create :restaurant
-      expect { create :restaurant }.to raise_exception ActiveRecord::RecordInvalid
-    end
+  it 'has a unique name' do
+    create :restaurant
+    expect { create :restaurant }.to raise_exception ActiveRecord::RecordInvalid
+  end
 
-    it 'as a unique slug' do
+  it 'has a unique slug' do
+    create :restaurant
+    expect { create :restaurant, :name => "dogdog" }.to raise_exception ActiveRecord::RecordInvalid
+  end
 
-    end
+  it 'is not valid without a description' do
+    expect { create :restaurant, :description => nil }.to raise_exception ActiveRecord::RecordInvalid
+  end
+
+  it 'has many items' do
+    item_1 = create :item, title: "Item1"
+    item_2 = create :item, title: "Item2"
+
+    item_1.restaurants << restaurant
+    item_2.restaurants << restaurant
+restaurant
+    assert item_1.restaurants.include?(restaurant)
+    assert item_2.restaurants.include?(restaurant)
   end
 end
