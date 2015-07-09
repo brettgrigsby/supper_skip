ff9d7def62e9a688e05e9685ef453f8937bcd54d
David Maurer <davemaurermail@gmail.com>
2015-07-08 18:53:46 -0600

    added restaurant create feature with tests

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,26 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
   def show
     @restaurant = Restaurant.find_by(slug: params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:success] = "You have created #{@restaurant.name}"
+      redirect_to restaurant_path(@restaurant)
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :slug)
   end
 end
