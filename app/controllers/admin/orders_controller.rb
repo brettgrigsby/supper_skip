b98fa308ba8859978dccfae004c564b74123811c
Rick Bacci <rickbacci@gmail.com>
2015-07-12 21:13:36 -0600

    add this stuff needs to be checked

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,11 +1,22 @@
 class Admin::OrdersController < AdminController
+  before_action :find_restaurant, only: [:index, :edit, :update, :new, :create]
+
   def index
     filter = known_scopes.find(-> { :all }) { |scope_name| scope_name == params[:scope] }
     @orders = Order.public_send filter
+    ## think restaurant_id needs to be in here
+   # @orders = Order.select { |orders| orders.restaurant_id == @restaurant.id }
   end
 
   def show
     @user_order = Order.find(params[:id])
+  end
+
+  def edit
+
+  end
+  def update
+    @orders = Order.public_send filter
   end
 
   def run_event
@@ -14,16 +25,19 @@ class Admin::OrdersController < AdminController
       user_order.public_send "#{params[:event]}!" # <--  dynamic method invocation *sigh* fkn metaprogramming, y'all
      # user_order.public_method("#{params[:event]}!").call#.call('some_argument_1', 'some_argument_2')
     end
-    redirect_to admin_orders_path(scope: user_order.aasm_state)
+    redirect_to admin_restaurant_orders_path(@restaurant, scope: user_order.aasm_state)
   end
 
   def delete_item
     item = Item.find(params[:item_id])
     @order = Order.find(params[:id])
     @order.remove_item(item)
-    redirect_to admin_order_path(@order.id)
+    redirect_to admin_restaurant_order_path(@restaurant, @order.id)
   end
 
+  def find_restaurant
+    @restaurant = Restaurant.find_by(slug: params[:restaurant_id])
+  end
   private
 
   def known_scopes
--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,8 +1,26 @@
 class Admin::RestaurantsController < AdminController
-  before_action :load_restaurant, only: [:show]
+  before_action :load_restaurant, only: [:create, :show]
   before_action :check_user, only: [:show]
 
   def show
+    @item = Item.find_by(restaurant_id: params[:restaurant_id])
+   # @item = Item.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    create_slug if @restaurant.slug.empty?
+    @restaurant.user = current_user
+    if @restaurant.save
+      current_user.role = 'admin'
+      current_user.save(validate: false)
+      flash[:success] = "You have created #{@restaurant.name}"
+
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash[:failure] = "There was a problem creating #{@restaurant.name}"
+      render :new
+    end
   end
 
   private
