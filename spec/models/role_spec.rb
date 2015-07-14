e14691fc8c492f22cfab9cf51de856e6ea67d970
brettgrigsby <gbrett@gmail.com>
2015-07-14 15:06:00 -0600

    add model test for role.users

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'responds with a collection of its users' do
+    role = Role.new(title: "user")
+
+    expect(role).to respond_to(:users)
+  end
 end
