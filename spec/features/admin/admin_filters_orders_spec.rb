96c0ff7705e6efe5dcfdb1271ea547f4723590aa
Rick Bacci <rickbacci@gmail.com>
2015-07-14 16:32:37 -0600

    fix tests for admin filtering orders
    - update view, remove un-needed code

--- spec/features/admin/admin_filters_orders_spec.rb
+++ spec/features/admin/admin_filters_orders_spec.rb
@@ -3,28 +3,19 @@ require_relative '../feature_spec_helper'
 describe "an admin filters orders by state" do
   include AdminHelper
 
-  let(:restaurant) { Restaurant.create!(name: 'testRest', description: 'passing or not', slug: 'slug me') }
-  # let(:paid_order) { Order.create!(delivery: true) }
-  #
-  # let(:ready_for_preparation_order) { Order.create!(delivery: true, workflow_state: :ready_for_preparation) }
-  # let(:cancelled_order) { Order.create!(delivery: true, workflow_state: :cancelled) }
-
-
-
   before do
     login_as_admin
     @restaurant = Restaurant.create!(name: 'testRest', description: 'passing or not', slug: 'slug me')
+
     @order = @restaurant.orders.create!(delivery: true)
     @cancelled_order = @restaurant.orders.create!(delivery: true, workflow_state: :cancelled)
     @paid_order = @restaurant.orders.create!(delivery: true)
-
-    # @paid_order = create :order, workflow_state: :paid
-    # @ready_for_preparation_order = create :order, workflow_state: :ready_for_preparation
-    # @in_preparation_order = create :order, workflow_state: :in_preparation
-    # @ready_for_delivery_order = create :order, workflow_state: :ready_for_delivery
-    # @out_for_delivery_order = create :order, workflow_state: :out_for_delivery
-    # #@cancelled_order = create! :order, workflow_state: :cancelled
-    # @delivered_order = create :order, workflow_state: :delivered
+    @ready_for_preparation_order = @restaurant.orders.create!(delivery: true, workflow_state: :ready_for_prep)
+    @in_preparation_order = @restaurant.orders.create!(delivery: true, workflow_state: :in_preparation)
+    @ready_for_delivery_order = @restaurant.orders.create!(delivery: true, workflow_state: :ready_for_delivery)
+    @out_for_delivery_order = @restaurant.orders.create!(delivery: true, workflow_state: :out_for_delivery)
+    @cancelled_order = @restaurant.orders.create!(delivery: true, workflow_state: :cancelled)
+    @delivered_order = @restaurant.orders.create!(delivery: true, workflow_state: :delivered)
     @all_orders = @restaurant.orders
   end
 
@@ -39,7 +30,7 @@ describe "an admin filters orders by state" do
   end
 
   it "sees all orders" do
-    visit admin_restaurant_order_path(@restaurant, @order, @order.workflow_state)
+    visit admin_restaurant_orders_path(@restaurant)
 
     within ".order-display" do
       all_orders.each do |order|
@@ -48,56 +39,125 @@ describe "an admin filters orders by state" do
     end
   end
 
-it "filters by cancelled" do
-   visit admin_restaurant_order_path(@restaurant, @cancelled_order, :cancelled)
-
-    within ".order-filters" do
-      click_on "Cancelled"
-    end
-
-    within ".order-display" do
-      expect(page).to have_content("Order ##{@cancelled_order.id}")
-      expect(page).not_to have_content "Order ##{@paid_order.id}"
-    end
-  end
-
 
   it "filters by paid" do
-    visit admin_restaurant_order_path(@restaurant.id, @paid_order, :paid, action: :index)
+    visit admin_restaurant_orders_path(@restaurant)
 
-    within ".order-filters" do
-      click_on "Paid"
-    end
+    find('button.paid').click
 
     within ".order-display" do
       expect(page).to have_content "Order ##{@paid_order.id}"
-      expect(page).not_to have_content "Order ##{@basket_order.id}"
+
+     # expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
     end
   end
 
-  xit "filters by complete" do
+  it "filters by ready for preparation" do
     visit admin_restaurant_orders_path(@restaurant)
 
-    within ".order-filters" do
-      click_on "Complete"
-    end
+    find('button.ready_for_prep').click
 
     within ".order-display" do
-      expect(page).to have_content("Order ##{@completed_order.id}")
-      expect(page).not_to have_content "Order ##{@paid_order.id}"
+      expect(page).to have_content("Order ##{@ready_for_preparation_order.id}")
+
+      expect(page).to have_css('.paid, .hidden')
+     # expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
     end
   end
+  it "filters by cancelled" do
+    visit admin_restaurant_orders_path(@restaurant)
 
-xit "filters by cancelled" do
-   visit admin_restaurant_order_path(@restaurant, @cancelled_order, :cancelled)
-
-    within ".order-filters" do
-      click_on "Cancelled"
-    end
+    find('button.cancelled').click
 
     within ".order-display" do
       expect(page).to have_content("Order ##{@cancelled_order.id}")
-      expect(page).not_to have_content "Order ##{@paid_order.id}"
+
+      expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+     # expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
+    end
+  end
+  it "filters by ready_for_in_preparation" do
+    visit admin_restaurant_orders_path(@restaurant)
+
+    find('button.in_preparation').click
+
+    within ".order-display" do
+      expect(page).to have_content("Order ##{@in_preparation_order.id}")
+
+      expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+     # expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
+    end
+  end
+  it "filters by ready_for_delivery" do
+    visit admin_restaurant_orders_path(@restaurant)
+
+    find('button.ready_for_delivery').click
+
+    within ".order-display" do
+      expect(page).to have_content("Order ##{@ready_for_delivery_order.id}")
+
+      expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+     # expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
+    end
+  end
+  it "filters by out_for_delivery" do
+    visit admin_restaurant_orders_path(@restaurant)
+
+    find('button.out_for_delivery').click
+
+    within ".order-display" do
+      expect(page).to have_content("Order ##{@out_for_delivery_order.id}")
+
+      expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+     # expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
+    end
+  end
+  it "filters by delivered" do
+    visit admin_restaurant_orders_path(@restaurant)
+
+    find('button.delivered').click
+
+    within ".order-display" do
+      expect(page).to have_content("Order ##{@delivered_order.id}")
+
+      expect(page).to have_css('.paid, .hidden')
+      expect(page).to have_css('.ready_for_prep, .hidden')
+      expect(page).to have_css('.in_preparation, .hidden')
+      expect(page).to have_css('.cancelled, .hidden')
+      expect(page).to have_css('.ready_for_delivery, .hidden')
+      expect(page).to have_css('.out_for_delivery, .hidden')
+      expect(page).to have_css('.delivered, .hidden')
     end
   end
 end
