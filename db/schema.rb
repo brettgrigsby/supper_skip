4b265943794ad5c28e4f8a972cf6acee21c31389
brettgrigsby <gbrett@gmail.com>
2015-07-14 16:43:30 -0600

    lots of broken tests from removing role column from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714205051) do
+ActiveRecord::Schema.define(version: 20150714223604) do
 
   create_table "addresses", force: true do |t|
     t.integer  "order_id"
@@ -134,7 +134,6 @@ ActiveRecord::Schema.define(version: 20150714205051) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
